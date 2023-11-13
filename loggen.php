ad9d7aeea902440e2bbecfbdc33befd43a12460c
Daniel Tildeman <78236594+dtildeman@users.noreply.github.com>
2023-11-13 22:09:57 +0200

    Add files via upload

---
+++ loggen.php
@@ -0,0 +1,463 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Strandnas Larm Logg</title> 
+</head> 
+2023-11-09 18:01:37 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning ikv&auml;ll kl 19:=
+00._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-11-09 12:02:37 - M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute. &Ouml;vrigt 3 ambula=
+nser ute._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-11-09 11:19:59 - Le11, Le14, Le17, M111, M3, M31, POLIS_B_411_Klass: Litet Alarm - Automatal=
+arm. &Ouml;vrig information Automatalarm._Skepparv&auml;gen 31, Skepparg=C3=
+=A5rden Pellas, Lemland
+
+
+<br><br>2023-11-02 18:03:15 - M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute, beredskap p&aring; de=
+p&aring;n._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-11-01 12:01:22 - PROVALARM-BEFOLKNINGSSKYDD MARIEHAMN_N_900_PROVALARM BEFOLKNINGSSKYDD MARIEHAMN. . Provalarm till Mariehamn ._Maarianhamina, Mariehamn
+
+
+<br><br>2023-10-26 18:01:14 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Strandn&auml;s FBK provalarm
+
+
+<br><br>2023-10-14 01:46:37 - Le11, Le14, Le17, M111, M161, M190, M3, M31, M371, M372, POLIS, PolisTeknik=
+_A_422_Klass: Grundalarm - Byggnadsbrand._Sk&ouml;nviksv&auml;gen, Marieham=
+n_anm bor i nr 12=20
+
+
+<br><br>2023-10-12 17:31:17 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Storagatan 5, Hotell Esplanad suites, Mariehamn
+
+
+<br><br>2023-10-06 17:51:14 - Le11, Le14, Le17, M111, M161, M190, M3, M31, M371, M372, POLIS, PolisTeknik=
+_A_422_Klass: Grundalarm - Byggnadsbrand._&Ouml;hbergsv&auml;gen 21 A, Mari=
+ehamn
+
+
+<br><br>2023-10-05 18:01:10 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning ikv&auml;ll kl 19.=
+_Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-09-28 19:43:24 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Styrmansgatan 2, Nyfahlers fastighets AB, Marieham
+
+
+<br><br>2023-09-28 18:00:40 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Strandn&auml;s, Maarianhamina
+
+
+<br><br>2023-09-27 14:54:02 - M3, M31, POLIS_B_411_Klass: Litet Alarm - Automatalarm._Nygatan 2, &Aring;la=
+ndsbanken, Mariehamn
+
+
+<br><br>2023-09-27 14:46:55 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Havsgatan 33, Mariepark, Mariehamn
+
+
+<br><br>2023-09-26 14:31:02 - M111, M3, M31, M371, M372, POLIS_A_421_Havsgatan 33, Mariepark, Mariehamn
+
+
+<br><br>2023-09-24 20:53:46 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.4999]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+X-Spam-Flag: NO
+
+M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute, &aring;k till MRV, be=
+redskap p&aring; dep&aring;n._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mar=
+iehamn
+
+
+<br><br>2023-09-21 18:02:44 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &ouml;vning ikv&auml;ll kl 19:=
+00._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-09-21 15:30:57 - M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute, beredskap p&aring; de=
+p&aring;n, kontakta f&ouml;rman tel 531485._Karlbergsv&auml;gen 12, Strandn=
+&auml;s FBK, Mariehamn
+
+
+<br><br>2023-09-20 07:37:38 - M31, M371, M372_C_440_Klass: Litet Alarm. H&auml;ndelse: Tr&auml;d blockera=
+r v&auml;gen helt. . / Typ av f&ouml;rst&auml;rkning Brandk&aring;r._Flakav=
+&auml;gen, Lemland_efter Hellestorp
+
+
+<br><br>2023-09-15 11:36:35 - Le11, Le14, Le17, Lu11, Lu17, M111, M3, M31, POLIS_A_421_Klass: Grundalarm =
+- Automatalarm._Lemlandsv&auml;gen 1865, Amalia, Lemland
+
+
+<br><br>2023-09-14 18:01:05 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-09-12 15:20:25 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Go=
+dbyv&auml;gen 13, Holmbergs sprinkleralarm, Mariehamn
+
+
+<br><br>2023-09-07 14:45:04 - M111, M3, M31, M371, M372, Marie_A_421_Klass: Grundalarm - Automatalarm._Nygatan 10-12, Hotell Savoy, Mariehamn
+
+
+<br><br>2023-09-06 10:27:26 - Le11, Le14, Le17, M111, M161, M190, M192, M3, M31, M371, M372, Marie, PolisTeknik_A_422_Klass: Grundalarm - Byggnadsbrand._Lotsgatan 6 b, Mariehamn
+
+
+<br><br>2023-08-31 18:01:15 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning kl 19:00._Karlberg=
+sv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-08-24 18:24:21 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Storagatan 5, Hotell Esplanad suites, Mariehamn
+
+
+<br><br>2023-08-18 18:08:19 - M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute, beredskap p&aring; de=
+p&aring;n._Strandn&auml;s FBK
+
+
+<br><br>2023-08-17 18:01:03 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Strandn&auml;s FBK provalarm
+
+
+<br><br>2023-08-17 07:49:28 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Do=
+ktorsv&auml;gen 1a, &Aring;lands centralsjukhus, Mariehamn
+
+
+<br><br>2023-08-15 09:29:50 - M31, StrFBK_C_441_Beredskapsalarm alla ambulanser ute._Strandn&auml;s FBK B=
+eredskap
+
+
+<br><br>2023-08-08 09:41:46 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Storagatan 5, Hotell Esplanad suites, Mariehamn
+
+
+<br><br>2023-08-02 12:55:02 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._V=
+&auml;stra Yttern&auml;sv&auml;gen, Baltichallen, Mariehamn
+
+
+<br><br>2023-07-30 00:34:31 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.5000]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+ -0.0 T_SCC_BODY_TEXT_LINE   No description available.
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+X-Spam-Flag: NO
+
+M31, StrFBK_C_441_Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-28 17:00:58 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Helgjouren inleds._Karlbergsv=
+&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-27 10:33:29 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._=
+&Ouml;stra Skolgatan 2, Handelsl&auml;roverket, Mariehamn
+
+
+<br><br>2023-07-24 19:19:22 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-23 20:33:22 - M111, M3, M31, M371, M372, POLIS_A_421_Neptunigatan 41, G&auml;sthem Neptun=
+, Mariehamn
+
+
+<br><br>2023-07-21 17:00:06 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Helgjouren inleds._Karlbergsv=
+&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-17 15:38:13 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute. beredskap p&aring; egen =
+dep&aring;._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-17 09:12:55 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Storagatan 5, Hotell Esplanad suites, Mariehamn
+
+
+<br><br>2023-07-16 03:08:31 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.5000]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+ -0.0 T_SCC_BODY_TEXT_LINE   No description available.
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+X-Spam-Flag: NO
+
+M31, StrFBK_C_441_Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-14 17:00:55 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . helgjouren inleds._Karlbergsv=
+&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-14 06:53:52 - M31, StrFBK_C_441_Beredskapsalarm alla enheter ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-13 19:54:35 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.5000]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+ -0.0 T_SCC_BODY_TEXT_LINE   No description available.
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+X-Spam-Flag: NO
+
+M31, StrFBK_C_441_Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-12 15:47:34 - Le11, Le14, Le17, M111, M190, M191, M3, M31, POLIS_B_430_Klass: R&auml;ddni=
+g - Assistans._Lemlandsv&auml;gen X Karl Manners v&auml;g, Lemland
+
+
+<br><br>2023-07-10 22:45:56 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Ne=
+ptunigatan 52, G&auml;sthem Kronan, Mariehamn
+
+
+<br><br>2023-07-09 13:02:15 - M3, M31, POLIS_B_411_Klass: Litet Alarm - Automatalarm._Matrosgatan 3, Tallbacken, Mariehamn
+
+
+<br><br>2023-07-09 11:44:40 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Havsgatan 33, Mariepark, Mariehamn
+
+
+<br><br>2023-07-08 16:45:43 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Havsgatan 33, Mariepark, Mariehamn
+
+
+<br><br>2023-07-07 17:11:44 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . helgjouren inleds._Karlbergsv=
+&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-07 09:30:00 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Sv=
+edgr&auml;nd 1, Gruppboende Svedgr&auml;nd, Mariehamn
+
+
+<br><br>2023-07-05 11:39:49 - M31, StrFBK_C_441_Beredskapsalarm 3 amb ute._Karlbergsv&auml;gen 12, Strand=
+n&auml;s FBK, Mariehamn
+
+
+<br><br>2023-07-01 22:33:12 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-06-29 20:50:29 - M31, MRV_C_441_Beredskapsalarm Alla ambulanser ute._Maarianhamina, Mariehamn
+
+
+<br><br>2023-06-27 08:04:40 - M111, M151, M190, M191, M3, M31, M371, M372, MRCC, POLIS, Sj&ouml;bevakning=
+en&Aring;land_D_432_Klass: R&auml;ddning - Dykalarm._Sj&ouml;promenaden, Mar=
+iehamn_vid &Aring;SS
+
+
+<br><br>2023-06-26 16:34:04 - M31, M371, M372_C_440_Typ av f&ouml;rst&auml;rkning Brandk&aring;r. / Klass:=
+ Grundalarm - Markbrand._F&ouml;gl&ouml;v&auml;gen 160, F&ouml;gl&ouml;
+
+
+<br><br>2023-06-23 17:00:36 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Provalarm till Strandn&auml;s =
+FBK, helgjouren inleds._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Marieha=
+mn
+
+
+<br><br>2023-06-17 07:09:14 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Na=
+bbv&auml;gen 12, Strandn&auml;s skola s&ouml;dra, Mariehamn
+
+
+<br><br>2023-06-14 17:51:48 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Do=
+ktorsv&auml;gen 1a, &Aring;lands centralsjukhus, Mariehamn
+
+
+<br><br>2023-06-14 14:38:05 - M31, MRV_C_441_Beredskapsalarm alla ambulanser ute._Strandn&auml;s, Maarian=
+hamina
+
+
+<br><br>2023-06-09 10:01:56 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-06-01 19:52:20 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Nygatan 10-12, Hotell Savoy, Mariehamn
+
+
+<br><br>2023-05-30 22:37:39 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-05-30 18:17:13 - M31, StrFBK_C_441_Beredskapsalarm 3 Ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-05-25 18:00:20 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Provalarm till Strandn&auml;s =
+FBK samt s&auml;songsavslutning kl 19.99._Karlbergsv&auml;gen 12, Strandn=
+&auml;s FBK, Mariehamn
+
+
+<br><br>2023-05-23 17:36:01 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Hamngatan 7, Hotell Adlon, Mariehamn
+
+
+<br><br>2023-05-23 09:58:53 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Norra Esplanadgatan 3, Park hotell, Mariehamn
+
+
+<br><br>2023-05-09 21:10:29 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.5000]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+ -0.0 T_SCC_BODY_TEXT_LINE   No description available.
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+X-Spam-Flag: NO
+
+Le11, Le14, Le17, M111, M161, M190, M3, M31, M371, M372, POLIS, PolisTeknik=
+_A_422_Klass: Grundalarm - Byggnadsbrand._Skrakv&auml;gen 23, Mariehamn
+
+
+<br><br>2023-05-09 15:02:33 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Gr=
+ind-Mattesv&auml;gen 1-3, Byggvaruhuset, Mariehamn
+
+
+<br><br>2023-05-08 10:39:42 - Le11, Le14, Le17, M111, M190, M191, M3, M31, POLIS_B_430_Klass: R&auml;ddni=
+g - Assistans._Flakav&auml;gen, Lemland_Mellan flaka o vester&auml;nga
+
+
+<br><br>2023-05-08 09:13:19 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Gr=
+ind-Mattesv&auml;gen 1-3, Byggvaruhuset, Mariehamn
+
+
+<br><br>2023-05-06 15:51:45 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-05-04 18:01:35 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &ouml;vning kl. 19._Karlbergsv=
+&auml;gen 12, Strandn&auml;s FBK, Mariehamn_ut kl 18
+
+
+<br><br>2023-05-04 09:38:56 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._S=
+&ouml;dragatan 18B, P&auml;rlan, Mariehamn
+
+
+<br><br>2023-05-02 13:47:29 - Le11, Le14, Le17, M111, M161, M190, M3, M31, M371, M372, POLIS, PolisTeknik=
+_A_422_Klass: Grundalarm - Byggnadsbrand._S&ouml;dra S&ouml;derbyv&auml;gen=
+ 70, Lemland
+
+
+<br><br>2023-04-27 18:00:58 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &ouml;vning i kv&auml;ll kl 19=
+._Strandn&auml;s, Maarianhamina
+
+
+<br><br>2023-04-27 11:44:15 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Hamngatan 1, Hotell Cikada, Mariehamn
+
+
+<br><br>2023-04-21 01:28:06 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Havsgatan 12, Miranda, Mariehamn
+
+
+<br><br>2023-04-20 18:00:54 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Strandn&auml;s FBK provalarm
+
+
+<br><br>2023-04-13 18:01:01 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning kl:19:00._Karlberg=
+sv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-04-10 14:25:30 - M111, M190, M193, M3, M31, M371, M372, POLIS_A_431_Klass: R&auml;ddning - F=
+astkl&auml;md._Nygatan 11, Mariehamn
+
+
+<br><br>2023-04-06 18:09:54 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Provalarm till Strandn&auml;s =
+FBK._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-04-06 08:42:26 - J15, Le11, Le14, Le17, M111, M191, M3, M31, M371, M372, POLIS_D_431_Klass: =
+R&auml;ddning - Fastkl&auml;md._Lemlandsv&auml;gen, Lemland_Vid bistorp sop=
+station
+
+
+<br><br>2023-04-05 14:31:50 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-04-04 10:56:53 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-04-03 11:47:24 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-04-02 06:49:07 - Le11, Le14, Le17, M111, M161, M190, M191, M193, M3, M31, M371, M372, POLIS, PolisTeknik, StrFBK_A_422_ /  / Klass: Grundalarm - Byggnadsbrand. /  / _Styrmansgatan 8, Maarianhamina
+
+
+<br><br>2023-04-02 06:24:36 -  Content analysis details:   (2.8 points, 5.0 required)
+  pts rule name              description
+ ---- ---------------------- --------------------------------------------------
+  0.8 BAYES_50               BODY: Bayes spam probability is 40 to 60%
+                             [score: 0.5002]
+ -0.0 SPF_PASS               SPF: sender matches SPF record
+  2.0 RDNS_NONE              Delivered to internal network by a host with no rDNS
+  0.0 KAM_DMARC_STATUS       Test Rule for DKIM or SPF Failure with Strict
+                             Alignment
+X-Spam-Flag: NO
+
+Le11, Le14, Le17, M111, M161, M190, M3, M31, M371, M372, POLIS, PolisTeknik_A_422_Klass: Grundalarm - Byggnadsbrand._Styrmansgatan 8, Maarianhamina
+
+
+<br><br>2023-03-31 11:44:38 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-30 18:02:23 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning ikv&auml;ll kl 19:=
+00._Strandn&auml;s FBK provalarm kl 18:00
+
+
+<br><br>2023-03-29 09:45:41 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-27 10:27:09 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute._Karlbergsv&auml;gen =
+12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-24 12:21:38 - M31, StrFBK_C_441_Beredskapsalarm Alla ambulanser ute, kontakta M05._Karlbe=
+rgsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-23 18:05:21 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-16 18:12:06 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . Provalarm till Strandn&auml;s =
+FBK samt &ouml;vning kl 19.00._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, =
+Mariehamn
+
+
+<br><br>2023-03-16 16:16:36 - M31, StrFBK_C_441_Beredskapsalarm 3 ambulanser ute._Karlbergsv&auml;gen 12,=
+ Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-12 17:56:14 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Norra Esplanadgatan 6, Trobergshemmet, Mariehamn
+
+
+<br><br>2023-03-11 11:15:10 - M31, MRV_C_441_Beredskapsalarm Alla ambulanser ute._Strandn&auml;s, Maarian=
+hamina
+
+
+<br><br>2023-03-09 18:00:52 - StrFBK_N_900_PROVALARM STRANDN&Auml;S FBK. . &Ouml;vning i kv&auml;ll kl 19=
+:00._Karlbergsv&auml;gen 12, Strandn&auml;s FBK, Mariehamn
+
+
+<br><br>2023-03-07 13:07:46 - M111, M3, M31, M371, M372, POLIS_A_421_Klass: Grundalarm - Automatalarm._Nygatan 10-12, Hotell Savoy, Mariehamn
+
+
+<br><br> 
+</html>
